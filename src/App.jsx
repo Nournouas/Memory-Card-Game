8f90b73e6d9674be79f8bbc1d0a0c6c17d413b6f
Nournouas <171356831+Nournouas@users.noreply.github.com>
2025-10-30 17:37:28 +0000

    Fix: Added nav basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react'
+import './index.css'
 import './App.css'
+import Nav from './Components/Nav'
+
 
 function App() {
-
+  const [scoreList, setScoreList] = useState([0]);
+  let bestScore = Math.max(...scoreList)
+  
   return (
     <>
+      <Nav score={scoreList[scoreList.length - 1]} bestScore={bestScore} />
     </>
   )
 }
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+export default function Nav({score, bestScore}){
+    return (
+        <nav>
+            <button>Reset</button>
+            <img className="nav-img" src="src\assets\ChatGPT Image Oct 30, 2025, 05_24_04 PM.png"/>
+            <div>
+                <p>Score: {score}</p>
+                <p>Best Score: {bestScore}</p>
+            </div>
+        </nav>
+    )
+}
